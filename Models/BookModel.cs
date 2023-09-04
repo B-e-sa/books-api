9eabae04969055d1cf503b5e3c858b61503335a4
Besa <joao.pereira@precato.com.br>
2023-09-04 12:48:35 -0300

    feat: adding AddedIn property

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,12 +1,15 @@
+
 namespace reader.Models
 {
     public class BookModel : IBookModel
     {
+        public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Author { get; set; }
         public string? Publisher { get; set; }
         public string? ReleaseDate { get; set; }
         public int? NumberOfPages { get; set; }
+        public DateTime AddedIn { get; set; }
     }
 }
--- Models/IBookModel.cs
+++ Models/IBookModel.cs
@@ -2,11 +2,13 @@ namespace reader.Models
 {
     public interface IBookModel
     {
+        public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Author { get; set; }
         public string? Publisher { get; set; }
         public string? ReleaseDate { get; set; }
         public int? NumberOfPages { get; set; }
+        public DateTime AddedIn { get; set; }
     }
 }
