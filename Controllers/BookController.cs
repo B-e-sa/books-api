512e5071d92a3d4a8751cf94971755db3b5c81b1
B-e-sa <joaospereira963@gmail.com>
2023-09-11 12:20:01 -0300

    feat: adding more https methods

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -14,9 +14,29 @@ public class BookController : ControllerBase
         _bookService = bookService;
     }
 
+    [HttpGet]
+    public async Task FindBook(int id)
+    {
+        await _bookService.FindById(id);
+    }
+
     [HttpPost]
     public async Task AddBook(Book book)
     {
         await _bookService.Add(book);
     }
+
+    [HttpPut]
+    public async Task EditBook(int id)
+    {
+        await _bookService.UpdateById(id);
+    }
+
+    [HttpDelete]
+    public async Task DeleteBook(int id)
+    {
+        await _bookService.DeleteById(id);
+    }
+
+
 }
