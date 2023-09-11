86d2557c4597b90993d20914fdf178f95ad76b80
B-e-sa <joaospereira963@gmail.com>
2023-09-11 12:18:40 -0300

    update: removing setters from id and addedIn properties

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,21 +1,17 @@
-
-using System.ComponentModel.DataAnnotations;
-using System.Numerics;
 using reader.Enums;
 
 namespace reader.Models
 {
     public class Book
     {
-        public int Id { get; set; }
+        public int Id { get; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Author { get; set; }
         public string? Publisher { get; set; }
         public string? ReleaseDate { get; set; }
         public int? NumberOfPages { get; set; }
-        public string? ISBN { get; set; }
         public ReadingStatus? ReadingStatus { get; set; }
-        public DateTime AddedIn { get; set; } = DateTime.Now;
+        public DateTime AddedIn { get; } = DateTime.Now;
     }
 }
